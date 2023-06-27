2d53f8d5187582294dd4daeb83702a2f56140b72
Fale King Nanmua <kingnanmuafale@gmail.com>
2023-06-27 03:58:46 -0700

    Update

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -6,6 +6,9 @@ import Police from '../../assets/police.png'
 import Crime from '../../assets/crime.png'
 import Logout from '../../assets/logout.png'
 import Officer from '../../assets/officer.png'
+import Arrow2  from '../../assets/arrow2.png'
+import Eye  from '../../assets/eye.png'
+import Plus  from '../../assets/Plus.png'
 
 
 const Sidebar = () => {
@@ -24,18 +27,19 @@ const Sidebar = () => {
     <div className='sidebar'>
       <div className="sidebar-container">
         <ul className='officer'><img src={Officer} alt="" /><span>Officer Salihu Musa</span></ul>
+        <li><img className='images' src={Visitors} alt="" /><a href="#"><span>Dashboard</span></a></li>
         <div>
-          <li onClick={handleDashboardDropdownClick}><img className='images' src={Dashboard} alt="" /> <span>Dashboard</span></li>
+          <li onClick={handleDashboardDropdownClick}><img className='images' src={Dashboard} alt="" /> <span>Criminal Record</span><img className='images' src={Arrow2} alt="" /></li>
           {isDashboardDropdownOpen&& (
             <ul>
-              <li>Option 1</li>
-              <li>Option 2</li>
+              <li><img className='images' src={Eye} alt="" />View Criminal Record</li>
+              <li><img className='images' src={Plus} alt="" />Add Criminal Record</li>
             </ul>
           )}
         </div>
 
         <div>
-          <li onClick={handleCriminalDropdownClick}><img className='images' src={Criminal} alt="" /><span>Criminal Record</span></li>
+          <li onClick={handleCriminalDropdownClick}><img className='images' src={Criminal} alt="" /><span>Visitor's Record</span><img className='images' src={Arrow2} alt="" /></li>
           {isCriminalDropdownOpen && (
             <ul>
               <li>Option 1</li>
@@ -43,8 +47,6 @@ const Sidebar = () => {
             </ul>
           )}
         </div>
-
-        <li><img className='images' src={Visitors} alt="" /><a href="#"><span>Visitors Record</span></a></li>
         <li><img className='images' src={Police} alt="" /><a href="#"><span>Police Profile</span></a></li>
         <li><img className='images' src={Crime} alt="" /><a href="#"><span>Crime Categories</span></a></li>
         <li><img className='images' src={Logout} alt="" /><a href="#"><span>Logout</span></a></li>
