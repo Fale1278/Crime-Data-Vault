80321290803ca804e5e24cac83282bf634960976
Fale King Nanmua <kingnanmuafale@gmail.com>
2023-07-04 03:14:00 -0700

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ const App = () => {
       {/* <ViewCriminal /> */}
       {/* <ViewVisitors /> */}
       {/* <AddVisitor /> */}
-      {/* <PoliceRecords /> */}
+      {/* <PoliceRecords />  */}
       <Footer />
     </div>
   )
